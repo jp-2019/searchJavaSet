3d2b3163c598a81b1c31347ce9fad694d030caf8
JP <49924656+jp-2019@users.noreply.github.com>
2019-11-07 10:19:41 -0500

    Add -1 if element doesnt exist in the list

--- Main.java
+++ Main.java
@@ -11,7 +11,14 @@ public class Main {
         // creating a new object but not adding it to the list
         // i can still search for it
         Customer James = new Customer(4, "James");
+        customers.add(James);
         if(customers.contains(James)) {
+            System.out.println("Object does exist");
+        } else {
+            System.out.println("Object does not exist");
+        }
+        // -1 is if the element doesnt exist in the list
+        if(customers.indexOf(James) != -1) {
             System.out.println("Object does exist");
         } else {
             System.out.println("Object does not exist");
